6409dda11df84128a6c573201bb3572a66a5b3f1
AlphaWan <3043777973@qq.com>
2025-09-19 09:41:48 +0800

    fix: 解决 JCE 加密库在服务器部署时的加载问题

--- yudao-server/src/main/java/cn/iocoder/yudao/server/YudaoServerApplication.java
+++ yudao-server/src/main/java/cn/iocoder/yudao/server/YudaoServerApplication.java
@@ -1,11 +1,14 @@
 package cn.iocoder.yudao.server;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.Security;
+
 /**
  * 项目的启动类
- *
+ * <p>
  * 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
  * 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
  * 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
@@ -16,19 +19,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"${yudao.info.base-package}.server", "${yudao.info.base-package}.module"})
 public class YudaoServerApplication {
 
+    /**
+     * 3. 在这里添加静态代码块
+     * 解决 Spring Boot Fat Jar 在高版本 JDK 下 JCE 无法验证 Bouncy Castle 的问题。
+     * 通过在程序启动的最早期，手动将 Bouncy Castle 添加到安全提供者列表中。
+     */
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
     public static void main(String[] args) {
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
         // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
 
         SpringApplication.run(YudaoServerApplication.class, args);
 //        new SpringApplicationBuilder(YudaoServerApplication.class)
 //                .applicationStartup(new BufferingApplicationStartup(20480))
 //                .run(args);
-
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
     }
 
 }
